7f255d623249304ea0eda238dbe885b2974e9ba6
katherine_rose <katherinerose56@gmail.com>
2016-02-01 23:31:06 +0000

    lab2

--- lab_1.cpp
+++ lab_1.cpp
@@ -20,27 +20,30 @@ using std::string;
  */
 void FirstLab() {
   // (1) Declare a string variable named first_name
-
+  string first_name;
   // (2) Declare a character variable named programmed_before and another named
   //     my_character. Initialize both of them to the value 'z'
-
+  char programmed_before;
+  char my_character;
+  programmed_before = 'z';
+  my_character = 'z';
   // (3) Display a welcome message to standard output
-
+  cout << "Hello.\n";
   // (4) Prompt the user for their first name
-
+  cout << "What is your name? Please input your name, then hit \'Enter\'.\n";
   // (5) Read the name from standard input and store in the variable first_name
-
+  cin >> first_name;
   // (6) Display the message "Nice to meet you, NAME" where NAME is replaced
   //     with the value of first_name
-
+  cout << "Nice to meet you, " << first_name << endl;
   // (7) Ask the user if they have programmed before. Make sure you inform them
   //     that you want the answer as a single character (y/n)
-
+  cout << "Have you ever programmed before? Enter \'y\' or \'n\'.\n";
   // (8): Read in the answer from standard input and store in the variable
   //      programmed_before
-
+  cin >> programmed_before;
   // (9): Call the function checkExperience appropriately so that the message will display
-
+  string checkExperience(char hasExperience);
 }
 
 /*
@@ -54,11 +57,20 @@ void FirstLab() {
 string checkExperience (char hasExperience) 
 {
     //CODE HERE
-    return string("");
+  if(hasExperience == 'y') {
+    return string("Welcome back!");
+} else {
+    return string("Get ready to have some fun!");
+  
 }
+
+}// end checkExperience
 
 int main()
 {
+  char hasExperience;
+  char programmed_before;
+  hasExperience = programmed_before;
   FirstLab();
   return 0;
 }
